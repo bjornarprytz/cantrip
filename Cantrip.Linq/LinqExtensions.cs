cf0e64a65ad1bbb99f5e9b5899d9887ae0a66016
Bjørnar Prytz <bjornarprytz@gmail.com>
2023-03-28 22:39:25 +0200

    initial commit

---
+++ Cantrip.Linq/LinqExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Cantrip.Linq;
+
+public static class LinqExtensions
+{
+    public static bool None<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        return !collection.Any(predicate);
+    }
+}
